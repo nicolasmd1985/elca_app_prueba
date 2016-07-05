68fff45e41077ffce99e4f74a63cff97de0062c7
nicolasmd1985 <nicolasmelectronic@gmail.com>
2016-07-04 23:37:29 -0300

    prueba

---
+++ app/src/main/java/com/example/nicolas/login/ReadComments.java
@@ -0,0 +1,21 @@
+package com.example.nicolas.login;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+/**
+ * Created by nicolas on 09/06/2016.
+ */
+public class ReadComments extends ActionBarActivity  {
+    @Override
+    protected void onCreate (Bundle saveInstanceState){
+        super.onCreate(saveInstanceState);
+        setContentView(R.layout.readcomments);
+
+
+    }
+
+}
+
+
+
